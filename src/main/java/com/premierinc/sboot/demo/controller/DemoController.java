70fd4b3ee302cc645d9db976ff4ca38c6e67838c
pramaswa <pavithra_ramaswamy@premierinc.com>
2016-07-12 16:16:24 -0400

    wip

--- src/main/java/com/premierinc/sboot/demo/controller/DemoController.java
+++ src/main/java/com/premierinc/sboot/demo/controller/DemoController.java
@@ -45,7 +45,7 @@ public class DemoController {
         if(!hasErrors){
             return "redirect:/demoPage";
         }else{
-            return "demopage";
+            return "DemoPage";
         }
     }
 }
